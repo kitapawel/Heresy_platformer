a0426029a7324730ad9bdc7b57a5f53f6bc36e4f
Paweł Kita <kitapawel@gmail.com>
2021-01-20 17:30:46 +0100

    Parallax background added

---
+++ Heresy-platformer/Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach to each parent background image
+//solution inspired by tutorial: https://www.youtube.com/watch?v=zit45k6CUMk
+public class ParallaxBackground : MonoBehaviour
+{
+    private float bckgrImageLength;
+    private float bckgrImageStartPosition;
+    public GameObject camera;
+    public float parallaxEffectAmount; //0 for no movement, 1 for speed equal to camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bckgrImageStartPosition = transform.position.x;
+        bckgrImageLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float offsetRelativeToCamera = camera.transform.position.x * (1 - parallaxEffectAmount);
+        float dist = camera.transform.position.x * parallaxEffectAmount;
+        
+        transform.position = new Vector3(bckgrImageStartPosition + dist, transform.position.y, transform.position.z);
+
+        if (offsetRelativeToCamera > bckgrImageStartPosition + bckgrImageLength)
+        {
+            bckgrImageStartPosition += bckgrImageLength;
+        } else if (offsetRelativeToCamera < bckgrImageStartPosition - bckgrImageLength)
+        {
+            bckgrImageStartPosition -= bckgrImageLength;
+        }
+
+    }
+}
